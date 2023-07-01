7a3d26c56be6826aeb2f52fa649b6359c1556e6b
Christophe Thibaut <cthibaut@octo.com>
2023-07-01 22:44:27 +0200

    no solution with target exceeding one coin

--- src/change.c
+++ src/change.c
@@ -6,6 +6,8 @@ int solutions_from(int start, int target, int max_coins, int *coins) {
         return 1;
     if(coins[start] > target)
         return 0;
+    if(coins[start] < target)
+        return solutions_from(0, target - coins[start], max_coins, coins);
 }
 int solutions(int target, int max_coins, int *coins) {
     return solutions_from(0, target, max_coins, coins);
--- test/change_tests.c
+++ test/change_tests.c
@@ -12,8 +12,9 @@ TEST_TEAR_DOWN(change) { }
 TEST(change, trivial_case) {
     int max_coins = 1;
     int coins[] = { 5 };
-    int result = solutions(4, max_coins, coins);
-    TEST_ASSERT_EQUAL(0, result);
+    TEST_ASSERT_EQUAL(0, solutions(4, max_coins, coins));
+    TEST_ASSERT_EQUAL(0, solutions(6, max_coins, coins));
+
 }
 
 TEST(change, simple_case) {
